79ae64e8123c36432acb72b22660cb05efe98077
Ewudes <ewudes@gmail.com>
2025-06-21 11:40:32 +0300

    fix: image task

--- gulp/tasks/image.js
+++ gulp/tasks/image.js
@@ -1,11 +1,13 @@
-const imagemin = require('gulp-imagemin'),
-  cache = require('gulp-cache'),
-  imgCompress  = require('imagemin-jpeg-recompress'),
-  imgPATH = {
-    "input": ["./dev/static/images/**/*.{png,jpg,gif,svg}",
-      '!./dev/static/images/svg/*'],
-    "output": "./build/static/images/"
-  };
+const imagemin = require('gulp-imagemin');
+const imgCompress = require('imagemin-jpeg-recompress');
+
+const imgPATH = {
+  input: [
+    './dev/static/images/**/*.{png,jpg,jpeg,gif,svg}',
+    '!./dev/static/images/svg/*'
+  ],
+  output: './build/static/images/'
+};
 
 module.exports = function () {
   $.gulp.task('img:dev', () => {
@@ -15,18 +17,22 @@ module.exports = function () {
 
   $.gulp.task('img:build', () => {
     return $.gulp.src(imgPATH.input)
-      .pipe(cache(imagemin([
+      .pipe(imagemin([
         imgCompress({
-            loops: 4,
-            min: 70,
-            max: 80,
-            quality: 'high'
+          loops: 4,
+          min: 70,
+          max: 80,
+          quality: 'high'
         }),
-        imagemin.gifsicle(),
-        imagemin.optipng(),
-        imagemin.svgo()
-      ])))
+        imagemin.gifsicle({ interlaced: true }),
+        imagemin.optipng({ optimizationLevel: 5 }),
+        imagemin.svgo({
+          plugins: [
+            { removeViewBox: false },
+            { cleanupIDs: false }
+          ]
+        })
+      ]))
       .pipe($.gulp.dest(imgPATH.output));
   });
 };
-
